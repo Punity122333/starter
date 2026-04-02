9697ceca2fcc939ef9e13c7512da85c8d01e80b8
Punity122333 <parthiv.pal.theheritageschool@gmail.com>
2026-04-03 02:24:39 +0530

    feat: removed testlua.lua

--- testlua.lua
+++
@@ -1,8 +0,0 @@
-
-for name, hl in pairs(vim.api.nvim_get_hl(0, {})) do 
-  local fg = hl.fg and string.format("#%06x", hl.fg) 
-  local bg = hl.bg and string.format("#%06x", hl.bg) 
-  if fg == "#111116" or bg == "#111116" then 
-    print(string.format("%s: fg=%s, bg=%s", name, fg or "none", bg or "none")) 
-  end 
-end
