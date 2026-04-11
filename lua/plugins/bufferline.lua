1fcf9b3528e55e13656aa6524da5abaaab5c297d
Punity122333 <parthiv.pal.theheritageschool@gmail.com>
2026-04-11 18:47:33 +0530

    feat: added some stuff

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,86 +1,15 @@
-local function goto_buffer(count)
-	require("bufferline").go_to(count, true)
-end
+local lettermap = { "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'" }
 
-local BufferKeys = {
-  {
-    "<leader>ba",
-    function()
-      goto_buffer(1)
-    end,
-    desc = "Go to buffer 1",
-  },
-	{
-    "<leader>bs",
-    function()
-      goto_buffer(2)
-    end,
-    desc = "Go to buffer 2",
-	},
-  {
-    "<leader>bd",
-    function()
-      goto_buffer(3)
-    end,
-    desc = "Go to buffer 3",
-  },
-  {
-    "<leader>bf",
-    function()
-      goto_buffer(4)
-    end,
-    desc = "Go to buffer 4",
-  },
-	{
-		"<leader>bg",
+local BufferKeys = {}
+for i, key in ipairs(lettermap) do
+	table.insert(BufferKeys, {
+		"<leader>b" .. key,
 		function()
-			goto_buffer(5)
+			require("bufferline").go_to(i, true)
 		end,
-		desc = "Go to buffer 5",
-	},
-	{
-		"<leader>bh",
-		function()
-			goto_buffer(6)
-		end,
-		desc = "Go to buffer 6",
-	},
-	{
-		"<leader>bj",
-		function()
-			goto_buffer(7)
-		end,
-		desc = "Go to buffer 7",
-	},
-	{
-		"<leader>bk",
-		function()
-			goto_buffer(8)
-		end,
-		desc = "Go to buffer 8",
-	},
-	{
-		"<leader>bl",
-		function()
-			goto_buffer(9)
-		end,
-		desc = "Go to buffer 9",
-	},
-	{
-		"<leader>b;",
-		function()
-			goto_buffer(10)
-		end,
-		desc = "Go to buffer 10",
-	},
-	{
-		"<leader>b'",
-		function()
-			goto_buffer(11)
-		end,
-		desc = "Go to buffer 11",
-	},
-}
+		desc = "Go to buffer " .. i,
+	})
+end
 
 local GodBg = "#1a1b26"
 local LightBlue = "#82aaff"
@@ -88,78 +17,73 @@ local LightBlue = "#82aaff"
 return {
 	{
 		"akinsho/bufferline.nvim",
+		lazy = false,
 		keys = BufferKeys,
-		opts = function()
-			return {
-				options = {
-					always_show_bufferline = false,
-					indicator = {
-						icon = "▎",
-						style = "icon",
-					},
-					separator_style = "thin",
-					diagnostics = "nvim_lsp",
-					numbers = function(numberopts)
-						local lettermap = { "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'" }
-						local letter = lettermap[numberopts.ordinal] or tostring(numberopts.ordinal)
-						return string.format("%d (%s)", numberopts.ordinal, letter)
-					end,
-					offsets = {
-						{
-							filetype = "neo-tree",
-							text = "file explorer",
-							highlight = "directory",
-							text_align = "left",
-							padding = 0,
-						},
-						{
-							filetype = "rconsole",
-							text = "r console",
-							highlight = "directory",
-							text_align = "left",
-							padding = 0,
-						},
-					},
-					filter_callback = function(buf_number, _)
-						if vim.bo[buf_number].filetype == "rconsole" then
-							return false
-						end
-						return true
-					end,
+		opts = {
+			options = {
+				always_show_bufferline = false,
+				indicator = {
+					icon = "▎",
+					style = "icon",
 				},
-				highlights = {
-					fill = { bg = GodBg },
-					background = { bg = GodBg },
-					separator = { fg = GodBg, bg = GodBg },
-					separator_visible = { fg = GodBg, bg = GodBg },
-					separator_selected = { fg = GodBg, bg = GodBg },
-					indicator_selected = {
-						fg = LightBlue,
-						bg = GodBg,
+				separator_style = "thin",
+				diagnostics = "nvim_lsp",
+				numbers = function(numberopts)
+					local letter = lettermap[numberopts.ordinal] or tostring(numberopts.ordinal)
+					return string.format("%d (%s)", numberopts.ordinal, letter)
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "file explorer",
+						highlight = "directory",
+						text_align = "left",
+						padding = 0,
 					},
-					buffer_visible = { bg = GodBg },
-					buffer_selected = {
-						bg = GodBg,
-						fg = LightBlue,
-						bold = true,
-						italic = false,
+					{
+						filetype = "rconsole",
+						text = "r console",
+						highlight = "directory",
+						text_align = "left",
+						padding = 0,
 					},
-					offset_separator = { fg = GodBg, bg = GodBg },
-					numbers = { fg = "#7aa2f7", bg = GodBg, bold = true },
-					numbers_selected = { fg = "#fa7355", bg = GodBg, bold = true },
-					close_button = { bg = GodBg },
-					close_button_visible = { bg = GodBg },
-					close_button_selected = { bg = GodBg },
-					modified = { bg = GodBg },
-					modified_visible = { bg = GodBg },
-					modified_selected = { bg = GodBg },
-					hint_visible = { bg = GodBg },
-					info_visible = { bg = GodBg },
-					warning_visible = { bg = GodBg },
-					error_visible = { bg = GodBg },
-					diagnostic_visible = { bg = GodBg },
 				},
-			}
-		end,
+				filter_callback = function(buf_number, _)
+					return vim.bo[buf_number].filetype ~= "rconsole"
+				end,
+			},
+			highlights = {
+				fill = { bg = GodBg },
+				background = { bg = GodBg },
+				separator = { fg = GodBg, bg = GodBg },
+				separator_visible = { fg = GodBg, bg = GodBg },
+				separator_selected = { fg = GodBg, bg = GodBg },
+				indicator_selected = {
+					fg = LightBlue,
+					bg = GodBg,
+				},
+				buffer_visible = { bg = GodBg },
+				buffer_selected = {
+					bg = GodBg,
+					fg = LightBlue,
+					bold = true,
+					italic = false,
+				},
+				offset_separator = { fg = GodBg, bg = GodBg },
+				numbers = { fg = "#7aa2f7", bg = GodBg, bold = true },
+				numbers_selected = { fg = "#fa7355", bg = GodBg, bold = true },
+				close_button = { bg = GodBg },
+				close_button_visible = { bg = GodBg },
+				close_button_selected = { bg = GodBg },
+				modified = { bg = GodBg },
+				modified_visible = { bg = GodBg },
+				modified_selected = { bg = GodBg },
+				hint_visible = { bg = GodBg },
+				info_visible = { bg = GodBg },
+				warning_visible = { bg = GodBg },
+				error_visible = { bg = GodBg },
+				diagnostic_visible = { bg = GodBg },
+			},
+		},
 	},
 }
